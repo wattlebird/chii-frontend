0a12ce73b1c667f7b749f31456587d4589278241
Ronnie Wang <geniusxiaoguai@gmail.com>
2023-11-24 19:43:53 +0800

    feat: enhanced frontpage

--- packages/app/src/routes/Main.tsx
+++ packages/app/src/routes/Main.tsx
@@ -1,20 +1,91 @@
 import * as React from 'react'
-import { styled } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { styled, useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 import { SearchBar } from '../components/advancedSearch'
 
 const Container = styled(Box)(({ theme }) => ({
-  marginTop: theme.spacing(4),
   display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  [theme.breakpoints.up('md')]: {
+    marginTop: '12%',
+    marginBottom: '20%',
+    marginLeft: theme.spacing(10),
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  [theme.breakpoints.down('md')]: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(2),
+  },
+}))
+
+const Background = styled(Box)(({ theme }) => ({
+  '& .shape1': {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    background: theme.palette.primary.main,
+    clipPath: 'polygon(1000% 0%,55% 0%,80% 100%,100% 100%)',
+    zIndex: -1,
+  },
+  '& .shape2': {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    background: theme.palette.primary.light,
+    clipPath: 'polygon(100% 0%,75% 0%,45% 100%,100% 100%)',
+    opacity: 0.5,
+    zIndex: -1,
+  },
+}))
+
+const SearchBarWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2),
 }))
 
 const Main: React.FunctionComponent = React.memo(() => {
+  const theme = useTheme()
+  const matchDesktop = useMediaQuery(theme.breakpoints.up('md'))
   return (
-    <Container>
-      <SearchBar simple sx={{ flexGrow: 1, maxWidth: 800 }} />
-    </Container>
+    <>
+      <Container>
+        <Typography variant={matchDesktop ? 'h1' : 'h2'} align='center' component='h1' gutterBottom fontWeight={600}>
+          再一次，定义搜索
+        </Typography>
+        <Typography variant='h4' component='h2' align='center' gutterBottom>
+          现已推出全新 Bangumi 搜索引擎，从这里开始你的探索
+        </Typography>
+        <SearchBarWrapper>
+          <SearchBar simple sx={{ flexGrow: 1, maxWidth: 800 }} />
+        </SearchBarWrapper>
+        <Stack spacing={2} direction='row'>
+          <Button variant='outlined' sx={{ borderRadius: 4 }} href='/search'>
+            高级搜索
+          </Button>
+          <Button variant='outlined' sx={{ borderRadius: 4 }} href='/rank'>
+            排行榜
+          </Button>
+        </Stack>
+      </Container>
+      <Background>
+        <div className='shape1' />
+        <div className='shape2' />
+      </Background>
+    </>
   )
 })
 
