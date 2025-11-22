22dde9e73904e36aea7cb265377493eaeb810f83
Ronnie Wang <geniusxiaoguai@gmail.com>
2025-11-22 22:39:08 +0800

    feat: locally test login feature

--- packages/app/src/store/router.tsx
+++ packages/app/src/store/router.tsx
@@ -3,8 +3,9 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import App from '../App'
 import { About } from '../routes/About'
 import { Ranking } from '../routes/Ranking'
-import { SearchPage } from '../routes/Search'
-import { Main } from '../routes/Main'
+import { SearchPage } from '../routes/Search';
+import { Main } from '../routes/Main';
+import Login from '../routes/Login';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,7 @@ export const router = createBrowserRouter([
       { path: 'about', element: <About /> },
       { path: 'search', element: <SearchPage /> },
       { path: 'rank', element: <Ranking /> },
+      { path: 'login', element: <Login /> },
       { path: '*', element: <Navigate to='/' replace={true} /> },
     ],
   },
